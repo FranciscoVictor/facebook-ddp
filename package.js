3d4e5e54ea3f608e8a016d642644132fe9c39f9a
Francisco De Souza Lopes <franciscovictor@outlook.com>
2016-02-18 23:48:45 -0200

    Package info and api's base version updated.
    Package info updated to match the version published by me at Atmosphere.

--- package.js
+++ package.js
@@ -1,17 +1,17 @@
 Package.describe({
-  name: 'jasperlu:facebook-ddp',
-  version: '0.0.2',
+  name: 'franciscovictor:facebook-ddp',
+  version: '0.0.3',
   // Brief, one-line summary of the package.
-  summary: "Facebook OAuth flow to include mobile ddp clients",
+  summary: 'OAuth authentication modified for mobile apps using DDP.',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/jasper-lu/facebook-ddp',
+  git: 'https://github.com/FranciscoVictor/facebook-ddp',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('METEOR@0.9.0');
+  api.versionsFrom('METEOR@1.2.1');
   api.use('oauth2', ['client', 'server']);
   api.use('oauth', ['client', 'server']);
   api.use('http', ['server']);
